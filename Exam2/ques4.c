9bc5996862562ee047507beecadda0229bdc0776
Heer-sketch <heer.k.jadav@gmail.com>
2024-12-21 10:47:25 +0530

    Create ques4.c

---
+++ Exam2/ques4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    char letter;
+    printf("Series:");
+    for(letter='A';letter<='Z';letter++)
+    {
+        printf("%c",letter);
+
+    }
+    return 0;
+    
+
+}
